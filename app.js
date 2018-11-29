c5fb6d10e9efe8f9a46eae78b757caaa2746df4a
Christopher Holley <holleyc7@me.com>
2018-11-29 15:43:58 -0500

    Adding in the settings to connect to the local mongoDB

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const comic = require('./routes/comic.route'); // Imports routes for the product
 const app = express();
 app.use('/comics', comic);
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://localhost:27017/ComicBookWebApp';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/comicss', comic);
+
 let port = 1234;
 
 app.listen(port, () => {
