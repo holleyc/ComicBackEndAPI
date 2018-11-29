03cd98d62507f7481de7b6d2ec652e4c93de2471
Christopher Holley <holleyc7@me.com>
2018-11-29 14:45:47 -0500

    Adding in the product model

---
+++ models/comic.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ComicSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+	publisher: {type: String, required: true, max: 100},
+    issue: {type: Number, required: true},
+});
+
+
+// Export the model
+module.exports = mongoose.model('Comic', ComicSchema);
